8fc8b1a7fcdf136f4b9fc44b1e1b5799b3126f30
ErlebnisW <7873787368748w@gmail.com>
2024-08-11 20:51:58 +0800

    use mirror & add wandb

--- examples/scripts/train_sft_llama.sh
+++ examples/scripts/train_sft_llama.sh
@@ -1,28 +1,33 @@
+export HF_ENDPOINT="https://hf-mirror.com"
+
 set -x
 
 read -r -d '' training_commands <<EOF
 openrlhf.cli.train_sft \
    --max_len 2048 \
-   --dataset Open-Orca/OpenOrca \
-   --input_key question \
+   --dataset PKU-Alignment/Align-Anything-Instruction-100K \
+   --input_key prompt \
    --output_key response \
    --train_batch_size 256 \
    --micro_train_batch_size 2 \
    --max_samples 500000 \
-   --pretrain meta-llama/Meta-Llama-3-8B \
+   --pretrain /home/wmz/checkpoints/llama3-8b \
    --save_path ./checkpoint/llama3-8b-sft \
    --save_steps -1 \
    --logging_steps 1 \
    --eval_steps -1 \
    --zero_stage 2 \
-   --max_epochs 1 \
+   --max_epochs 3 \
    --bf16 \
    --flash_attn \
-   --learning_rate 5e-6 \
+   --learning_rate 4e-5 \
    --load_checkpoint \
-   --gradient_checkpointing
+   --gradient_checkpointing \
+   --use_wandb true \
+   --wandb_project Formal-SFT \
+   --wandb_run_name llama3-align-anything 
 EOF
-    # --use_wandb [WANDB_TOKENS]
+    
     # --packing_samples
 
 if [[ ${1} != "slurm" ]]; then
