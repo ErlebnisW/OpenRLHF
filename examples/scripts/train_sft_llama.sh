203f04b3897a683165f918047c0b81d33a5867f4
ErlebnisW <7873787368748w@gmail.com>
2024-08-12 00:00:59 +0800

    set default sft dataset as openorca

--- examples/scripts/train_sft_llama.sh
+++ examples/scripts/train_sft_llama.sh
@@ -6,11 +6,11 @@ read -r -d '' training_commands <<EOF
 openrlhf.cli.train_sft \
    --max_len 2048 \
    --dataset Open-Orca/OpenOrca \
-   --input_key prompt \
+   --input_key question \
    --output_key response \
    --train_batch_size 256 \
    --micro_train_batch_size 2 \
-   --max_samples 500000 \
+   --max_samples 50000 \
    --pretrain /home/wmz/checkpoints/llama3-8b \
    --save_path ./checkpoint/llama3-8b-sft \
    --save_steps -1 \
